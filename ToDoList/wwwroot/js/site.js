dd7c62c026fc88199c02db40fd349b9931bef6d0
JoAnisky <anisky5@gmail.com>
2022-11-12 10:57:49 +0100

    JS : Listener Btn delete

--- ToDoList/wwwroot/js/site.js
+++ ToDoList/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function InitDeleteListener() {
+    const deleteBtn = document.querySelectorAll(".delete-btn");
+    deleteBtn.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            let taskNumber = e.target.dataset.taskId;
+            DeleteTodo(taskNumber)
+        })
+    })
+
+}
+
+function DeleteTodo(id) {
+    let taskNumber = id;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    InitDeleteListener();
+});
